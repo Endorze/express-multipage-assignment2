2e7e82ebf1d6326e62d185c2accd92c3addfc9ce
Endorze <alexyurihallgren@hotmail.com>
2025-02-12 18:01:56 +0100

    Work work

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    res.render("pages/home", {
+    res.render("pages/homes", {
         pageTitle: "Home",
     })
 });
