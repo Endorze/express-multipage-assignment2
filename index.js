6c1bf02bb690f5b6a78ad2f3cc7eb0d04bd6fa6b
Endorze <alexyurihallgren@hotmail.com>
2025-02-12 18:11:05 +0100

    work work

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ dotenv.config();
 const port = process.env.PORT || 3000;
 
 const app = express();
-app.use(express.static("public"));
+// app.use(express.static("public"));
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
 
 app.set("view engine", "ejs");
 
