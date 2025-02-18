1ac56b63222866b0e7df839b02ed521b412eba33
Endorze <alexyurihallgren@hotmail.com>
2025-02-18 16:59:12 +0100

    feat: Worked on filestructure, added dynamical generated content to collections page.

---
+++ data/collection.js
@@ -0,0 +1,31 @@
+function Character(imgPath, iconPath, heroName, stanceType, grade, health, attack, defence, speed, awakenBonus) {
+    this.imgPath = imgPath;
+    this.iconPath = iconPath;
+    this.heroName = heroName;
+    this.stanceType = stanceType;
+    this.grade = grade;
+    this.health = health;
+    this.attack = attack;
+    this.defence = defence;
+    this.speed = speed;
+    this.awakenBonus = awakenBonus;
+}
+
+let characterList = [
+    new Character("/image/collectionPage/characters/ethna/ethna.gif", "/image/collectionPage/characters/ethna/ethnaicon.png", "Ethna", "Attack", "5 Star", "10380", "845", "604", "119", "Increases Crit Rate by 15%"),
+    new Character("/image/collectionPage/characters/alicia/alicia.gif", "/image/collectionPage/characters/alicia/aliciaicon.png", "Alicia", "Attack", "5 Star", "10380", "845", "604", "119", "Increases Crit Rate by 15%"),
+    new Character("/image/collectionPage/characters/laika/laika.gif", "/image/collectionPage/characters/laika/laikaicon.png", "Laika", "Attack", "5 Star", "10380", "845", "604", "119", "Increases Crit Rate by 15%"),
+    new Character("/image/collectionPage/characters/laika/laika.gif", "/image/collectionPage/characters/laika/laikaicon.png", "Laika", "Attack", "5 Star", "10380", "845", "604", "119", "Increases Crit Rate by 15%"),
+    new Character("/image/collectionPage/characters/laika/laika.gif", "/image/collectionPage/characters/laika/laikaicon.png", "Laika", "Attack", "5 Star", "10380", "845", "604", "119", "Increases Crit Rate by 15%"),
+    new Character("/image/collectionPage/characters/laika/laika.gif", "/image/collectionPage/characters/laika/laikaicon.png", "Laika", "Attack", "5 Star", "10380", "845", "604", "119", "Increases Crit Rate by 15%"),
+    new Character("/image/collectionPage/characters/laika/laika.gif", "/image/collectionPage/characters/laika/laikaicon.png", "Laika", "Attack", "5 Star", "10380", "845", "604", "119", "Increases Crit Rate by 15%"),
+    new Character("/image/collectionPage/characters/laika/laika.gif", "/image/collectionPage/characters/laika/laikaicon.png", "Laika", "Attack", "5 Star", "10380", "845", "604", "119", "Increases Crit Rate by 15%"),
+    new Character("/image/collectionPage/characters/laika/laika.gif", "/image/collectionPage/characters/laika/laikaicon.png", "Laika", "Attack", "5 Star", "10380", "845", "604", "119", "Increases Crit Rate by 15%"),
+    new Character("/image/collectionPage/characters/laika/laika.gif", "/image/collectionPage/characters/laika/laikaicon.png", "Laika", "Attack", "5 Star", "10380", "845", "604", "119", "Increases Crit Rate by 15%"),
+    new Character("/image/collectionPage/characters/laika/laika.gif", "/image/collectionPage/characters/laika/laikaicon.png", "Laika", "Attack", "5 Star", "10380", "845", "604", "119", "Increases Crit Rate by 15%"),
+    new Character("/image/collectionPage/characters/laika/laika.gif", "/image/collectionPage/characters/laika/laikaicon.png", "Laika", "Attack", "5 Star", "10380", "845", "604", "119", "Increases Crit Rate by 15%"),
+]
+
+
+
+export default characterList;
